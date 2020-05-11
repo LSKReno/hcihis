604bdf65b2cf40e215e0d2962e679b725036a90a
happym <1320355349@qq.com>
2020-05-11 21:26:51 +0800

    final

---
+++ src/views/medicalRecordManage/OverallTabPane.vue
@@ -0,0 +1,35 @@
+<template>
+    <div style="margin-left: 20px; margin-top: 5px;">
+        <el-tabs v-model="activePageName" @tab-click="changePageTab">
+            <el-tab-pane label="查看患者病历" name="browseMedRecord">
+
+            </el-tab-pane>
+            <el-tab-pane label="编辑病历" name="editMedRecord">
+            </el-tab-pane>
+            <el-tab-pane label="检查检验项目" name="medicalProject">
+            </el-tab-pane>
+            <el-tab-pane label="处方" name="prescription">
+            </el-tab-pane>
+        </el-tabs>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'OverallTabPane',
+        data () {
+            return{
+
+            }
+        },
+        methods: {
+            changePageTab () {
+
+            }
+        }
+    }
+</script>
+
+<style>
+
+</style>
---
+++ src/views/medicalRecordManage/prescription/prescription.vue
@@ -0,0 +1,39 @@
+<template>
+    <div>
+        <div style="margin-left: 10px; margin-right: 10px; width: 60%; height: 500px;
+                    border-style: solid; border-color: lightgray;
+                    border-top-width: 0px; border-right-width: 1px;
+                    border-bottom-width: 0px; border-left-width: 0px; float: left">
+        </div>
+        <div style="margin-left: 10px; margin-right: 10px; width: 35%; height: 100px; float: right">
+        </div>
+    </div>
+</template>
+
+<script>
+
+    export default {
+        name: 'description',
+        data () {
+            return {
+
+            }
+        },
+        methods: {
+
+        },
+        computed: {
+
+        },
+        props: {
+
+        },
+        components: {
+
+        }
+    }
+</script>
+
+<style>
+
+</style>
