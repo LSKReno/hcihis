f8b6f900dc89c9d0fc7c13b9d44d7320a7c7e92e
LSKReno <18236198930@163.com>
2020-05-11 23:10:32 +0800

    feat: 敏感信息去除

--- src/config/app.js
+++ src/config/app.js
@@ -1,9 +1,6 @@
 import localKey from './localKey'
 
 // const devApiUrl = 'http://127.0.0.1:9081';
-// const devApiUrl = 'http://49.232.136.80:9081'
-
-// const proApiUrl = 'http://49.232.136.80:9081'
 // const proApiUrl = 'http://192.168.49.196:10003';
 
 const nodeDevEnv = process.env.NODE_ENV === 'development'
